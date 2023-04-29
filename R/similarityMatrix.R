9dbdd7d84e1eab48ffdfc1111d0f16da46e9bee3
Andrew Chin <chinandrew96@gmail.com>
2023-04-29 14:06:12 -0400

    add test and refactor sliding functions into own file

--- R/similarityMatrix.R
+++ R/similarityMatrix.R
@@ -94,22 +94,6 @@ similarityMatrix <- function(x,
 
 }
 
-#' @noRd
-#'
-sliding_cov2 <- function(long, short) {
-  n = length(short)
-  len_diff = length(long) - n
-  convolve_cpp(long, rev(short / (n - 1) - sum(short) / n / (n - 1)))[n:(n + len_diff)]
-}
-
-#' @noRd
-#'
-sliding_cor2 <- function(long, short) {
-  sliding_cor_cpp(short, long, sd(short))
-}
-
-
-
 
 #' Template Index Matrix Computation
 #'

40ee0d40305026bc96968ab3a6cfc5cca4c5852b
Andrew Chin <chinandrew96@gmail.com>
2023-04-30 16:53:20 -0400

    fix test description

--- tests/testthat/test-sliding.R
+++ tests/testthat/test-sliding.R
@@ -1,13 +1,13 @@
 context("Testing sliding cov and cor functions.")
 
-test_that("sliding_cov2 agrees with dvmisc::sliding_cov", {
+test_that("sliding_cov_fast agrees with dvmisc::sliding_cov", {
   long <- c(1, 5, 2, 7, 4, 5)
   short <- c(1.5, 1, 2.1)
   expect_equal(dvmisc::sliding_cov(short, long),
                sliding_cov_fast(short, long))
 })
 
-test_that("sliding_cor2 agrees with dvmisc::sliding_cov", {
+test_that("sliding_cor_fast agrees with dvmisc::sliding_cov", {
   long <- c(1, 5, 2, 7, 4, 5)
   short <- c(1.5, 1, 2.1)
   expect_equal(dvmisc::sliding_cor(short, long),

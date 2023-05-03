1f413d0a96bcba319da082b9f3d9e2a593b6b050
chinandrew <chinandrew96@gmail.com>
2023-05-03 14:37:12 -0400

    Update test for sliding cov to use mean 0 sd 1 short vec

--- tests/testthat/test-sliding.R
+++ tests/testthat/test-sliding.R
@@ -2,7 +2,7 @@ context("Testing sliding cov and cor functions.")
 
 test_that("sliding_cov_fast agrees with dvmisc::sliding_cov", {
   long <- c(1, 5, 2, 7, 4, 5)
-  short <- c(1.5, 1, 2.1)
+  short <- c(-1, 0, 1)
   expect_equal(dvmisc::sliding_cov(short, long),
                sliding_cov_fast(short, long))
 })

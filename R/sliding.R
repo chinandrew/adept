8856a558b5e62c5c62186f388d3d0141e19a74a3
Andrew Chin <chinandrew96@gmail.com>
2023-05-02 22:58:31 -0400

    fft

--- R/sliding.R
+++ R/sliding.R
@@ -8,6 +8,15 @@ sliding_cov_fast <- function(short, long) {
 
 #' @noRd
 #'
+sliding_cov_fft <- function(short, long) {
+  n <- length(short)
+  len_diff <- length(long) - n
+  return(convolve(long, c(short / (n - 1), rep(0,len_diff)))[1:(len_diff+1)])
+}
+
+
+#' @noRd
+#'
 sliding_cor_store_sd <- function(short, long) {
   return(sliding_cor_store_sd_cpp(short, long))
 }

08a033324d70c584c93b54f59e0c68aaf8cdd9d1
chinandrew <chinandrew96@gmail.com>
2023-05-04 19:47:05 -0400

    Add cor test

--- tests/testthat/test-sliding.R
+++ tests/testthat/test-sliding.R
@@ -6,3 +6,11 @@ test_that("sliding_cov_fast agrees with dvmisc::sliding_cov when short has mean 
   expect_equal(dvmisc::sliding_cov(short, long),
                sliding_cov_fast(short, long))
 })
+
+
+test_that("sliding_cor_store_sd agrees with dvmisc::sliding_cov when short has mean 0 and sd 1", {
+  long <- c(1, 5, 2, 7, 4, 5)
+  short <- c(-1, 0, 1)
+  expect_equal(dvmisc::sliding_cor(short, long),
+               sliding_cor_store_sd(short, long)$cor)
+})

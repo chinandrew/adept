ca7747a36da0019dfebfa76933e51186dba06445
chinandrew <chinandrew96@gmail.com>
2023-05-04 18:15:29 -0400

    Update test name

--- tests/testthat/test-sliding.R
+++ tests/testthat/test-sliding.R
@@ -1,6 +1,6 @@
 context("Testing sliding cov and cor functions.")
 
-test_that("sliding_cov_fast agrees with dvmisc::sliding_cov", {
+test_that("sliding_cov_fast agrees with dvmisc::sliding_cov when short has mean 0 and sd 1", {
   long <- c(1, 5, 2, 7, 4, 5)
   short <- c(-1, 0, 1)
   expect_equal(dvmisc::sliding_cov(short, long),
